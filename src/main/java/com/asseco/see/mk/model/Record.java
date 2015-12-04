0fb6e049defd231d8e038031b745b2c3f0a1f169
Ivo Shurbanovski <ivo.shurbanovski@asseco-see.mk>
2015-12-04 15:26:13 +0100

    Second Commit
    Modification of model classes.

--- src/main/java/com/asseco/see/mk/model/Record.java
+++ src/main/java/com/asseco/see/mk/model/Record.java
@@ -2,16 +2,17 @@ package com.asseco.see.mk.model;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 @Entity
@@ -19,230 +20,236 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 public class Record implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 
-	@ManyToMany
-	private Set<Client> clients;
+	@ManyToOne
+	private Client client;
 
-	@ManyToMany
-	private Set<Sector> sectors;
+	@ManyToOne
+	private Sector sector;
 
-	@ManyToMany
-	private Set<Project> projects;
+	@ManyToOne
+	private Project project;
 
-	@ManyToMany
-	private Set<Seller> sellers;
+	@ManyToOne
+	private Seller seller;
 
-	@ManyToMany
-	private Set<Status> statuses;
-	
-	@Column(name="plan_date")
+	@ManyToOne
+	private Status status;
+
+	@ManyToOne
+	private Reservation reservation;
+
+	@Column(name = "plan_date")
 	private Date pDate;
-	
-	@Column(name="plan_cogs")
+
+	@Column(name = "plan_cogs")
 	private float pCogs;
-	
-	@Column(name="plan_opex")
+
+	@Column(name = "plan_opex")
 	private float pOpex;
-	
-	@Column(name="provision_cost_date")
-	private Date cpDate;
-	
-	@Column(name="provision_cost_cogs")
-	private float cpCogs;
-	
-	@Column(name="closing_provision_opex")
-	private float cpOpex;
-	
-	@Column(name="closing_provision_date")
+
+	@Column(name = "provision_cost_date")
+	private Date provCostDate;
+
+	@Column(name = "provision_cost_cogs")
+	private float provCostCogs;
+
+	@Column(name = "closing_provision_opex")
+	private float provCostOpex;
+
+	@Column(name = "closing_provision_date")
 	private Date closingProvisionDate;
-	
-	@Column(name="closing_provision_cogs")
+
+	@Column(name = "closing_provision_cogs")
 	private float closingProvisionCogs;
-	
-	@Column(name="closing_provision_opex")
+
+	@Column(name = "closing_provision_opex")
 	private float closingProvisionOpex;
-	
-	@Column(name="remaining_amount_cogs")
+
+	@Column(name = "remaining_amount_cogs")
 	private float remAmountCogs;
-	
-	@Column(name="remainig_mmount_opex")
+
+	@Column(name = "remainig_mmount_opex")
 	private float remAmountOpex;
-	
 
 	public Record() {
 	}
 
-
-	public Set<Client> getClients() {
-		return clients;
+	public Client getClient() {
+		return client;
 	}
 
-
-	public void setClients(Set<Client> clients) {
-		this.clients = clients;
+	public void setClient(Client client) {
+		this.client = client;
 	}
 
-
-	public Set<Sector> getSectors() {
-		return sectors;
+	public Sector getSector() {
+		return sector;
 	}
 
-
-	public void setSectors(Set<Sector> sectors) {
-		this.sectors = sectors;
+	public void setSector(Sector sector) {
+		this.sector = sector;
 	}
 
-
-	public Set<Project> getProjects() {
-		return projects;
+	public Project getProject() {
+		return project;
 	}
 
-
-	public void setProjects(Set<Project> projects) {
-		this.projects = projects;
+	public void setProjects(Project project) {
+		this.project = project;
 	}
 
-
-	public Set<Seller> getSellers() {
-		return sellers;
+	public Seller getSeller() {
+		return seller;
 	}
 
-
-	public void setSellers(Set<Seller> sellers) {
-		this.sellers = sellers;
+	public void setSeller(Seller seller) {
+		this.seller = seller;
 	}
 
-
-	public Set<Status> getStatuses() {
-		return statuses;
+	public Status getStatus() {
+		return status;
 	}
 
-
-	public void setStatuses(Set<Status> statuses) {
-		this.statuses = statuses;
+	public void setStatus(Status status) {
+		this.status = status;
 	}
-
 
 	public Date getpDate() {
 		return pDate;
 	}
 
-
 	public void setpDate(Date pDate) {
 		this.pDate = pDate;
 	}
-
 
 	public float getpCogs() {
 		return pCogs;
 	}
 
-
 	public void setpCogs(float pCogs) {
 		this.pCogs = pCogs;
 	}
-
 
 	public float getpOpex() {
 		return pOpex;
 	}
 
-
 	public void setpOpex(float pOpex) {
 		this.pOpex = pOpex;
 	}
 
-
 	public Date getCpDate() {
-		return cpDate;
+		return provCostDate;
 	}
-
 
 	public void setCpDate(Date cpDate) {
-		this.cpDate = cpDate;
+		this.provCostDate = cpDate;
 	}
-
 
 	public float getCpCogs() {
-		return cpCogs;
+		return provCostCogs;
 	}
-
 
 	public void setCpCogs(float cpCogs) {
-		this.cpCogs = cpCogs;
+		this.provCostCogs = cpCogs;
 	}
-
 
 	public float getCpOpex() {
-		return cpOpex;
+		return provCostOpex;
 	}
-
 
 	public void setCpOpex(float cpOpex) {
-		this.cpOpex = cpOpex;
+		this.provCostOpex = cpOpex;
 	}
-
 
 	public Date getClosingProvisionDate() {
 		return closingProvisionDate;
 	}
 
-
 	public void setClosingProvisionDate(Date closingProvisionDate) {
 		this.closingProvisionDate = closingProvisionDate;
 	}
-
 
 	public float getClosingProvisionCogs() {
 		return closingProvisionCogs;
 	}
 
-
 	public void setClosingProvisionCogs(float closingProvisionCogs) {
 		this.closingProvisionCogs = closingProvisionCogs;
 	}
-
 
 	public float getClosingProvisionOpex() {
 		return closingProvisionOpex;
 	}
 
-
 	public void setClosingProvisionOpex(float closingProvisionOpex) {
 		this.closingProvisionOpex = closingProvisionOpex;
 	}
-
 
 	public float getRemAmountCogs() {
 		return remAmountCogs;
 	}
 
-
 	public void setRemAmountCogs(float remAmountCogs) {
 		this.remAmountCogs = remAmountCogs;
 	}
-
 
 	public float getRemAmountOpex() {
 		return remAmountOpex;
 	}
 
-
 	public void setRemAmountOpex(float remAmountOpex) {
 		this.remAmountOpex = remAmountOpex;
 	}
 
-
 	public int getId() {
 		return id;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(id).append(sector).append(client).append(project).append(seller)
+				.append(status).append(pDate).append(pCogs).append(pOpex).append(provCostDate).append(provCostCogs)
+				.append(provCostOpex).append(closingProvisionDate).append(closingProvisionCogs)
+				.append(closingProvisionOpex).append(remAmountCogs).append(remAmountOpex).append(reservation)
+				.toHashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Record))
+			return false;
+		if (obj == this)
+			return true;
+
+		Record rhs = (Record) obj;
+		return new EqualsBuilder().append(id, rhs.id).append(sector, rhs.sector).append(project, rhs.project)
+				.append(seller, rhs.seller).append(status, rhs.status).append(pDate, rhs.pDate).append(pCogs, rhs.pCogs)
+				.append(pOpex, rhs.pOpex).append(provCostDate, rhs.provCostDate).append(provCostCogs, rhs.provCostCogs)
+				.append(provCostOpex, rhs.provCostOpex).append(closingProvisionDate, rhs.closingProvisionDate)
+				.append(closingProvisionCogs, rhs.closingProvisionCogs)
+				.append(closingProvisionOpex, rhs.closingProvisionOpex).append(remAmountCogs, rhs.remAmountCogs)
+				.append(remAmountOpex, rhs.remAmountOpex).append(reservation, rhs.reservation).isEquals();
+	}
+
 	@Override
 	public String toString() {
-		return new ToStringBuilder(this).append("id", this.id).append("sectors", this.sectors).append("clients", this.clients).append("projects",this.projects).append("sales persons", this.sellers).append("plan date", this.pDate).append("plan cogs(eur)",this.pCogs).append("").toString();
+		return new ToStringBuilder(this).append("id", this.id).append("sector", this.sector)
+				.append("client", this.client).append("project", this.project).append("sales person", this.seller)
+				.append("status", this.status).append("plan date", this.pDate).append("plan cogs(eur)", this.pCogs)
+				.append("plan opex(eur)", this.pOpex).append("provision cost date", this.provCostDate)
+				.append("provision cost cogs(eur)", this.provCostCogs)
+				.append("provision cost opex(eur)", this.provCostOpex)
+				.append("closing provision date", this.closingProvisionDate)
+				.append("closing provision cogs(eur)", this.closingProvisionCogs)
+				.append("closing provision opex(eur)", this.closingProvisionOpex)
+				.append("remaining amount COGS(eur)", this.remAmountCogs)
+				.append("remainng amount OPEX(eur)", this.remAmountOpex).append("reservation", this.reservation)
+				.toString();
 	}
 }
--- src/main/java/com/asseco/see/mk/model/Reservation.java
+++ src/main/java/com/asseco/see/mk/model/Reservation.java
@@ -2,13 +2,19 @@ package com.asseco.see.mk.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
 
 @Entity
 @Table(name = "reservations")
@@ -20,7 +26,7 @@ public class Reservation implements Serializable {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 
-	@Column(name = "String")
+	@Column(name = "reservation_name")
 	private String name;
 
 	@Column(name = "modified_by")
@@ -29,7 +35,71 @@ public class Reservation implements Serializable {
 	@Column(name = "date_modified")
 	private Date dateModified;
 
+	@OneToMany(mappedBy = "reservation")
+	private Set<Record> records;
+
 	public Reservation() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public Date getDateModified() {
+		return dateModified;
+	}
+
+	public void setDateModified(Date dateModified) {
+		this.dateModified = dateModified;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Set<Record> getRecords() {
+		return records;
+	}
+
+	public void setRecords(Set<Record> records) {
+		this.records = records;
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(id).append(name).append(modifiedBy).append(dateModified).append(records)
+				.toHashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Reservation))
+			return false;
+		if (obj == this)
+			return true;
+
+		Reservation rhs = (Reservation) obj;
+		return new EqualsBuilder().append(id, rhs.id).append(name, rhs.name).append(modifiedBy, rhs.modifiedBy)
+				.append(dateModified, rhs.dateModified).append(records, rhs.records).isEquals();
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this).append("id", this.id).append("name", this.name)
+				.append("modified by", this.modifiedBy).append("date of modification", this.dateModified)
+				.append("records", this.records).toString();
 	}
 
 }
